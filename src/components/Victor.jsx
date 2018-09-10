85fdac108930c739087c462f05d7d56431c94281
Kevin <kjh7796@gmail.com>
2018-09-09 20:31:44 -0400

    Refactoring to stateless component

--- src/components/Victor.jsx
+++ src/components/Victor.jsx
@@ -7,8 +7,6 @@ const Victor = () => {
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Welcome to Victors site</h1>
-
-        <h1 className="App-title">Welcome to Victors site</h1>
         <h4 className="">
           <Link to="/">Click Here to go home!</Link>
         </h4>
