4b2a0250dbb21213ae7478176570257bc375762a
Kevin <kjh7796@gmail.com>
2018-09-09 20:07:14 -0400

    Test eslint

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import PropTypes from 'prop-types'; // When I have to pass in props uncomment this
+import PropTypes from 'prop-types'; // When I have to pass in props uncomment this
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Victor from './components/Victor';
 import HomePage from './components/HomePage'
