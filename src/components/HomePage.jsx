81bf9fad9aac1771b41b56ec53bf4f2556843f46
Kevin <kjh7796@gmail.com>
2018-09-09 20:46:03 -0400

    Refactoring to stateless component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,19 @@
+// eslint jsx-one-expression-per-line: "error"
+
 import React from 'react';
 // import PropTypes from 'prop-types'; // When I have to pass in props uncomment this
 import './HomePage.css';
 import { Link } from 'react-router-dom';
 
-class HomePage extends React.Component {
-  render () {
-    return(
-      <div className="App">
-          <div><h1><Link to="/victor">Victor Route</Link></h1></div>
-          THIS IS THE HOMEPAGE!
-      </div>
-
-    );
-  }
-}
+const HomePage = () => {
+  return (
+    <div>
+      <h1>
+        HomePage, press
+        <Link>Here</Link> to go to Victor route
+      </h1>
+    </div>
+  );
+};
 
 export default HomePage;
